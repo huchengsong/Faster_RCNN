17546f78952cde8137c894ffae3c6645aee705b7
huchengsong <huchengsong0814@gmail.com>
2018-03-22 17:32:08 -0400

    contiguous

--- faster_rcnn_vgg16.py
+++ faster_rcnn_vgg16.py
@@ -174,8 +174,8 @@ class VGG16ROIHead(nn.Module):
         # TODO: just for testing
         from Mask_head import roi_align
         pool_result = roi_align(x, rois, img_size, self.pool_size, sub_sample=2)
+        pool_result = pool_result.contiguous()
         pool_result = pool_result.view(pool_result.size()[0], -1)
-
         fc = self.classifier(pool_result)
         roi_cls_locs = self.cls_loc(fc)
         roi_scores = self.score(fc)
